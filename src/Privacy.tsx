ceb206fd4d5311ca54b01e240a85437d80ffec64
KingChampion36 <26169430+KingChampion36@users.noreply.github.com>
2026-03-08 17:40:03 +0530

    Fix build

--- src/Privacy.tsx
+++ src/Privacy.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function getTheme(): "light" | "dark" {
   return document.documentElement.getAttribute("data-theme") === "light"
